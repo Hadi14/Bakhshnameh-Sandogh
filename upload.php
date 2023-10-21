5b000282c4d7ec672644f5a38f9a81ec48136048
Hadi14 <asusn43@yahoo.com>
2023-10-21 14:17:57 +0330

    some changes2

--- upload.php
+++ upload.php
@@ -12,14 +12,14 @@
 </head>
 
 <body>
-    <form action="" method="post" enctype="multipart/form-data">
-        <label for="title">عنوان فایل</label>
-        <input id="title" type="text">
-        فایل خود را برای آپلود انتخاب کنید.
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input style="border: 1px solid black !important;" type="submit" value="ارسال فایل" name="submit">
+    <div class="frm  py-5">
+        <form action="" method="post" enctype="multipart/form-data">
+            فایل خود را برای آپلود انتخاب کنید.
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input style="border: 1px solid black !important;" type="submit" value="ارسال فایل" name="submit">
 
-    </form>
+        </form>
+    </div>
     <div class="container-table100">
 
         <div class="wrap-table100">
