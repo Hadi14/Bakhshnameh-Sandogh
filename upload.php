bed248d316df9a6cb78874263ce4f187a0937704
Hadi <asusn43@yahoo.com>
2023-10-22 21:59:49 +0330

    correct upload  and rename file

--- upload.php
+++ upload.php
@@ -3,7 +3,92 @@ session_start();
 if (!isset($_SESSION['uname'])) {
     header("Location:index.php");
 }
-// echo "USENAME :" . $_SESSION['uname'];
+if (isset($_GET['upfile'])) {
+    uploadPdf();
+    //     echo "<script type='text/javascript'>
+    // $(document).ready(function(){
+    // $('#Modal').modal('show');
+    // $('#notemodal').text('ok?');
+    // });
+    // </script>";
+} elseif (isset($_GET['rename'])) {
+    renamePDF();
+}
+function renamePDF()
+{
+    $n = $_POST['oldname'];
+    $path = "./uploads/";
+    $oldname = preg_replace('/\s+/u', ' ', $n);
+    $old = $path .   $oldname;
+    $new = $path . $_POST['newName'] . ".pdf";
+    rename($old, $new);
+}
+
+function uploadPdf()
+{
+    $target_dir = "uploads/";
+    $pureName = basename($_FILES["fileToUpload"]["name"]);
+    $sepName = explode('.', $pureName);
+
+    $fileName = $sepName[0] . '.pdf';
+    $target_file = $target_dir . $fileName;
+
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+
+    if (!($_FILES["fileToUpload"]["name"])) {
+        echo "Sorry, No file selected. Please Select a file.";
+        $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 20000000) {
+        echo  $_FILES['fileToUpload']['size'] .  "******Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if (
+        $imageFileType != "pdf"
+    ) {
+        echo "Sorry, only PDF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
+    /****************************************** */
+    // echo "<br>";
+    // $dirpath  = __DIR__ . "/../../uploads";
+    // $files = scandir($dirpath);
+
+    // foreach ($files as $file) {
+
+    //     $filePath = __DIR__ . "\/..\/..\/uploads" . '/' . $file;
+    //     if (is_file($filePath)) {
+    //         $rest = str_replace(' ', '&nbsp;', $file);
+    //         $path = __DIR__ . "/../../uploads/" . $file;
+    //         echo "<a class='dl'  href='" . getBaseUrl() . "downloadfile/downloadPDF/$file' >" . $file . "</a>" . "&nbsp;&nbsp;&nbsp;" . "<a class='del'  href='" . getBaseUrl() . "upload/RemovePDF/$file' >Delete</a>" . "&nbsp;&nbsp;&nbsp;" . "<a data-bs-toggle='modal' data-bs-target='#renameModal' class='del'  onclick=LoadnameFile('$rest') href='' >Rename</a>";
+    //         echo "<br>";
+    //     }
+    // }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,7 +112,7 @@ if (!isset($_SESSION['uname'])) {
         </div>
         <div class="logout"></div>
         <div class="frm  py-5">
-            <form action="" method="post" enctype="multipart/form-data">
+            <form action="upload.php?upfile=up" method="post" enctype="multipart/form-data">
                 فایل خود را برای آپلود انتخاب کنید.
                 <input type="file" name="fileToUpload" id="fileToUpload">
                 <input style="border: 1px solid black !important;" type="submit" value="ارسال فایل" name="submit">
@@ -90,7 +175,7 @@ if (!isset($_SESSION['uname'])) {
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form method="post" action="upload/renamePDF">
+                    <form method="post" action="upload.php?rename=true">
                         <div class="mb-0">
                             <label id="otherfiledlabel" for="recipient-name1" class="col-form-label">نام فایل:</label>
                             <input id="otherrecipientName1" name="newName" type="text" class="form-control">
@@ -98,6 +183,28 @@ if (!isset($_SESSION['uname'])) {
                         </div>
                         <div class="modal-footer">
                             <input type="submit" class="btn btn-primary" data-bs-dismiss="modal" value="ویرایش">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">خروج</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--------------------------------- End of Modal ----------------------------------------------------------->
+    <!--------------------------- status upload modal  ------------------------------------------------------------------------->
+    <div class="modal fade" id="statusModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" dir="rtl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="otherModalLabel"> </h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="post" action="upload/renamePDF">
+                        <div class="mb-0">
+                            <label id="notemodal" for="recipient-name1" class="col-form-label"></label>
+                        </div>
+                        <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">خروج</button>
                         </div>
                     </form>
