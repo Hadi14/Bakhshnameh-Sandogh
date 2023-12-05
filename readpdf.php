d704679b09669ea5c14d0c926fd36cc3dbda5a9e
Hadi14 <asusn43@yahoo.com>
2023-12-05 10:29:12 +0330

    show in frame and add back btn

--- readpdf.php
+++ readpdf.php
@@ -1,11 +1,9 @@
-<button>بازگشت</button>
+<link rel="stylesheet" href="css/bootstrap.rtl.min.css">
+<div class="" style="position: sticky; top:0; display:flex; justify-content:center; padding:10px; ">
+
+    <a class="btn btn-primary" href="index.php" role="button">بازگشت</a>
+</div>
+
+
+<iframe src="./showpdf.php?file=<?= $_GET['file'] ?>" frameborder="0" width="100%" height="100%"></iframe>
 <?
-$fileNm =  rawurldecode($_GET['file']);
-$fileName = basename($fileNm);
-$filePath = "./uploads/$fileNm";
-/***************************************** */
-header("Content-Type: application/pdf");
-header("Content-Disposition:inline; filename=$fileName");
-header("Content-Transfer-Encoding: binary");
-header("Accept-Ranges:bytes");
-@readfile($filePath);
